938e24b85dc5cefbb08b0d51593052c482b6e9d2
Ben Lee <bendelonlee@gmail.com>
2019-01-01 18:23:36 -0700

    Adds US 8

--- spec/features/trip_show_spec.rb
+++ spec/features/trip_show_spec.rb
@@ -11,20 +11,28 @@ describe 'the trip show page' do
     within "#longest-trail" do
       expect(page).to have_content("Longest Trail on Trip: #{trail_1.name}")
       expect(page).to have_content("#{trail_1.length} miles")
+      expect(page).to have_link(trail_1.name, href: trail_path(trail_1))
     end
     within "#shortest-trail" do
       expect(page).to have_content("Shortest Trail on Trip: #{trail_2.name}")
       expect(page).to have_content("#{trail_2.length} miles")
+      expect(page).to have_link(trail_2.name, href: trail_path(trail_2))
+
     end
     within "#trail-#{trail_1.id}" do
       expect(page).to have_content(trail_1.name)
+      expect(page).to have_link(trail_1.name, href: trail_path(trail_1))
       expect(page).to have_content(trail_1.address)
       expect(page).to have_content("#{trail_1.length} miles")
     end
     within "#trail-#{trail_2.id}" do
       expect(page).to have_content(trail_2.name)
+      expect(page).to have_link(trail_2.name, href: trail_path(trail_2))
+
       expect(page).to have_content(trail_2.address)
       expect(page).to have_content("#{trail_2.length} miles")
     end
   end
+
+
 end
